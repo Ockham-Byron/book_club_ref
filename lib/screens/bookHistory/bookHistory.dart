b6b4c095bca5ec1c13daa14f25d60f59ba87bc56
Ockham-Byron <ockham-byron@protonmail.com>
2022-01-21 10:55:56 +0100

    bookHistory

--- lib/screens/bookHistory/bookHistory.dart
+++ lib/screens/bookHistory/bookHistory.dart
@@ -5,6 +5,7 @@ import 'package:book_club_ref/models/userModel.dart';
 import 'package:book_club_ref/screens/root/root.dart';
 import 'package:book_club_ref/services/auth.dart';
 import 'package:book_club_ref/services/dbFuture.dart';
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 
 import 'package:flutter/material.dart';
 
@@ -38,6 +39,42 @@ class _BookHistoryState extends State<BookHistory> {
     super.didChangeDependencies();
   }
 
+  String _displayGroupName() {
+    if (widget.currentGroup.name != null) {
+      return widget.currentGroup.name!;
+    } else {
+      return "Groupe sans nom";
+    }
+  }
+
+  bool withProfilePicture() {
+    if (widget.currentUser.pictureUrl == "") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  Widget displayCircularAvatar() {
+    if (withProfilePicture()) {
+      return CircularProfileAvatar(
+        widget.currentUser.pictureUrl,
+        showInitialTextAbovePicture: false,
+      );
+    } else {
+      return CircularProfileAvatar(
+        "https://digitalpainting.school/static/img/default_avatar.png",
+        foregroundColor: Theme.of(context).focusColor.withOpacity(0.5),
+        initialsText: Text(
+          widget.currentUser.pseudo![0].toUpperCase(),
+          style: TextStyle(
+              fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        showInitialTextAbovePicture: true,
+      );
+    }
+  }
+
   void _signOut(BuildContext context) async {
     String _returnedString = await Auth().signOut();
     if (_returnedString == "success") {
@@ -54,62 +91,95 @@ class _BookHistoryState extends State<BookHistory> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.groupName,
-          style: TextStyle(
-            color: Colors.white,
-          ),
-          textAlign: TextAlign.center,
+      body: Container(
+        alignment: Alignment.bottomCenter,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover),
         ),
-        backgroundColor: Theme.of(context).primaryColor,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.outbond_rounded,
-              color: Colors.white,
-            ),
-            onPressed: () => _signOut(context),
-          )
-        ],
-      ),
-      body: FutureBuilder(
-        future: books,
-        builder:
-            (BuildContext context, AsyncSnapshot<List<BookModel>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return ListView.builder(
-              itemCount: snapshot.data!.length + 1,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return Text("");
-                  // return Padding(
-                  //   padding: const EdgeInsets.all(10.0),
-                  //   child: Row(
-                  //     children: [
-                  //       BackButton(),
-                  //     ],
-                  //   ),
-                  // );
-                } else {
-                  return Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: EachBook(
-                      book: snapshot.data![index - 1],
-                      groupId: widget.groupId,
-                      currentGroup: widget.currentGroup,
-                      currentUser: widget.currentUser,
-                    ),
-                  );
-                }
-              },
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+        child: FutureBuilder(
+          future: books,
+          builder:
+              (BuildContext context, AsyncSnapshot<List<BookModel>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Builder(
+                                    builder: (context) => GestureDetector(
+                                      child: displayCircularAvatar(),
+                                      onTap: () =>
+                                          Scaffold.of(context).openDrawer(),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  width: 200.0,
+                                  decoration: new BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      color: Colors.grey.shade200
+                                          .withOpacity(0.5)),
+                                  child: Text(
+                                    _displayGroupName(),
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  Icons.logout_rounded,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () => _signOut(context),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: EachBook(
+                        book: snapshot.data![index - 1],
+                        groupId: widget.groupId,
+                        currentGroup: widget.currentGroup,
+                        currentUser: widget.currentUser,
+                      ),
+                    );
+                  }
+                },
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
       ),
     );
   }
